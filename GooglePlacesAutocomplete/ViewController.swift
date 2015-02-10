fc7fa32cdd1e4d0ad129195bad9288ab212d657d
Howard Wilson <howard@watsonbox.net>
2015-02-10 12:10:51 +0100

    New Single Page Application XCode 6.1.1

---
+++ GooglePlacesAutocomplete/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GooglePlacesAutocomplete
+//
+//  Created by Howard Wilson on 10/02/2015.
+//  Copyright (c) 2015 Howard Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
