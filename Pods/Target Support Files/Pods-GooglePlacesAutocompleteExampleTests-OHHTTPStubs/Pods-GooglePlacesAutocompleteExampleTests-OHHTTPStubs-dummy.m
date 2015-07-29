1d0b52dca2ae5c78ed9250e82b454e28cfe981aa
Sam Easterby-Smith <sam@spotlightkid.co.uk>
2015-07-29 12:11:42 +0100

    Fixed cocoapods disasters with test dependencies

--- Pods/Target Support Files/Pods-GooglePlacesAutocompleteExampleTests-OHHTTPStubs/Pods-GooglePlacesAutocompleteExampleTests-OHHTTPStubs-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GooglePlacesAutocompleteExampleTests_OHHTTPStubs : NSObject
-@end
-@implementation PodsDummy_Pods_GooglePlacesAutocompleteExampleTests_OHHTTPStubs
-@end
